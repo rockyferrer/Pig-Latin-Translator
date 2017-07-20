0c5cf792fdef45c724bea9c651bf76e897325892
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-07-20 19:32:18 -0400

    Init commit

---
+++ piglatin/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+VOWELS = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def translate(request):
+    original = request.GET['originaltext']
+    result = ''
+    for word in original.split():
+        if word.startswith(VOWELS):  # starts with vowel
+            result += word + 'yay'
+        else:  # starts with consonant
+            result += word[1:] + word[0] + 'ay'
+
+    return render(request, original)
